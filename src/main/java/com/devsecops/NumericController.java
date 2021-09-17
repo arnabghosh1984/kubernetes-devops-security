5fe52dd1920b3daf6bdf140db4fdc630fe8cc50e
Arnab <arnabghosh1984@gmail.com>
2021-09-17 15:02:06 +0530

    Update NumericController.java

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -35,8 +35,7 @@ public class NumericController {
 		public String compareToFifty(@PathVariable int value) {
 			String message = "Could not determine comparison";
 			if (value > 50) {
-				message = 50 ; /* Customized code */
-
+				message = "Greater than 50";
 			} else {
 				message = "Smaller than or equal to 50";
 			}
